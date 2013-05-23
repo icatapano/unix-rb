51d56f3c302d5aefb6d4eeec1f2f5f6dca5384cc
Casey Robinson <kc@rampantmonkey.com>
2013-05-23 11:08:08 -0400

    class for parsing options to tail

--- lib/unix_rb/tail.rb
+++ lib/unix_rb/tail.rb
@@ -1,5 +1,8 @@
 require_relative '../unix_rb'
 
+require 'optparse'
+require 'pathname'
+
 module UnixRb
   class Tail
     def initialize opts={}
@@ -33,6 +36,43 @@ module UnixRb
 
       def method_missing m, *a, &b
         @config.fetch(m) { super }
+      end
+  end
+
+  class TailOptions
+    attr_reader :values
+
+    def initialize argv
+      @values = Hash.new
+      parse argv
+    end
+
+    private
+      def parse argv
+        OptionParser.new do |opts|
+          opts.banner = "Usage: tail [options] [file]"
+          opts.on("-h", "--help", "Show this message") do
+            puts opts
+            exit
+          end
+          opts.on("-n number", Integer, "number of lines to display") do |n|
+            @values[:number] = n
+          end
+
+          begin
+            opts.parse!(argv)
+          rescue OptionParser::ParseError => e
+            STDERR.puts e.message, "\n", opts
+            exit 1
+          end
+        end
+
+        if argv.empty?
+          @values[:input] = STDIN
+        else
+          file = Pathname.new(argv.pop)
+          file.exist? ? @values[:input] = file : fail( MissingFileError, "#{file} does not exist")
+        end
       end
   end
 end
