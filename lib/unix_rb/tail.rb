ea19f58d3463ea1d195e9b9ebad85f738c3a41fc
Casey Robinson <kc@rampantmonkey.com>
2013-05-23 09:03:55 -0400

    tail now works for strings and files

--- lib/unix_rb/tail.rb
+++ lib/unix_rb/tail.rb
@@ -9,7 +9,28 @@ module UnixRb
                 }.merge! opts
     end
 
+    def run
+      open.each("\n")
+          .entries
+          .reverse
+          .take(number)
+          .reverse
+          .join
+    end
+
     private
+      def open
+        i = input
+        case i
+        when String
+          StringIO.new i
+        when Pathname
+          i.open("r")
+        else
+          i
+        end
+      end
+
       def method_missing m, *a, &b
         @config.fetch(m) { super }
       end
--- spec/lib/unix_rb/tail_spec.rb
+++ spec/lib/unix_rb/tail_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../../spec_helper'
+require 'pathname'
 
 module UnixRb
   describe Tail do
@@ -20,6 +21,31 @@ module UnixRb
     it "has an operating mode" do
       t = Tail.new
       t.mode
+    end
+
+    context "#run" do
+      it "should show all ten lines of input" do
+        t = Tail.new({ input: "line\n"*10 })
+        t.run.should eq "line\n"*10
+      end
+
+      it "should show only the last ten lines" do
+        t = Tail.new({ input: "line\n"*42 })
+        t.run.should eq "line\n"*10
+      end
+
+      it "should only show the last number of lines" do
+        t = Tail.new({ input: "line\n"*42, number: 3})
+        t.run.should eq "line\n"*3
+      end
+
+      it "works with files" do
+        f = Pathname.new("testing")
+        f.open("w:UTF-8"){|o| o.puts "line\n"*12 }
+        t = Tail.new({ input: f })
+        t.run.should eq "line\n"*10
+        f.unlink
+      end
     end
   end
 end
