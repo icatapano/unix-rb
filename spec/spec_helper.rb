06eee2d9a8c27b1284988aecf8843f43ff93a321
Casey Robinson <kc@rampantmonkey.com>
2013-05-23 09:04:47 -0400

    create a fake standard input stream for testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,17 @@
 require_relative '../lib/unix_rb'
 
+module Helpers
+  # Fake stdin with StringIO
+  def fake_stdin(*args)
+    begin
+      $stdin = StringIO.new
+      $stdin.puts(args.shift) until args.empty?
+      $stdin.rewind
+      yield
+    ensure
+      $stdin = STDIN
+    end
+  end
+end
+
+RSpec.configure {|conf| conf.include(Helpers)}
